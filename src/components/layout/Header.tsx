65cbb75b861a35ea9f9da63316fcbc58c1433d86
Mohamad Al-Kahfi <91003256+KahfiSmith@users.noreply.github.com>
2025-08-16 18:38:41 +0700

    feat: update Header component for improved theming and accessibility

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,27 +1,28 @@
 import { Button } from "@/components/ui/button";
+import { Coffee } from "lucide-react";
 import { Link } from "react-router-dom";
 
 export const Header = () => {
   return (
     <div className="text-center mt-10">
-      <div className="p-8 rounded-lg bg-soft-coral/80 border-3 border-coral">
-        <h1 className="text-4xl text-coral mb-3 flex items-center gap-3 font-bold justify-center">
-          <img src="icons/coffee.svg" alt="" className="w-10 h-10" />
+      <div className="p-8 rounded-lg bg-soft-coral/80 border-3 border-coral dark:border-cream dark:bg-coal-400">
+        <h1 className="text-4xl text-coral dark:text-cream mb-3 flex items-center gap-3 font-bold justify-center">
+          <Coffee className="w-10 h-10 fill-coral/70 dark:fill-cream/80" />
           Code Roaster
-          <img src="icons/coffee.svg" alt="" className="w-10 h-10" />
+          <Coffee className="w-10 h-10 fill-coral/70 dark:fill-cream/80" />
         </h1>
-        <p className="text-coral text-lg font-bold mb-2">
+        <p className="text-coral dark:text-cream text-lg font-bold mb-2 ">
           Code issue adalah caraku mengekspresikan bahwa kodingan lo sangat
           ampasss.
         </p>
-        <div className="text-coral/90 text-sm font-bold tracking-wide">
+        <div className="text-coral dark:text-cream text-sm font-bold tracking-wide">
           Kopi panas, review gw lebih panas.
         </div>
       </div>
 
       <div className="mt-8 flex justify-center space-x-6 mb-8">
         <Link to="/">
-          <Button className="bg-sky hover:bg-sky/70 text-charcoal font-medium py-1.5 px-3 rounded-md border-2 border-charcoal transition-all duration-200 shadow-[3px_3px_0px_0px_#27292b] hover:shadow-[1px_1px_0px_0px_#27292b] hover:translate-x-[2px] hover:translate-y-[2px] active:shadow-none active:translate-x-[3px] active:translate-y-[3px] text-sm">
+          <Button className="bg-sky hover:bg-sky/80 text-charcoal font-medium py-1.5 px-3 rounded-md border-2 border-charcoal transition-all duration-200 shadow-[3px_3px_0px_0px_#27292b] hover:shadow-[1px_1px_0px_0px_#27292b] hover:translate-x-[2px] hover:translate-y-[2px] active:shadow-none active:translate-x-[3px] active:translate-y-[3px] text-sm">
             <img src="icons/house.svg" alt="" className="w-6 h-6" />
             <span>Code Reviewer</span>
           </Button>
@@ -33,7 +34,7 @@ export const Header = () => {
           </Button>
         </Link> */}
         <Link to="/history">
-          <Button className="bg-coral hover:bg-coral/70 text-charcoal font-medium py-1.5 px-3 rounded-md border-2 border-charcoal transition-all duration-200 shadow-[3px_3px_0px_0px_#27292b] hover:shadow-[1px_1px_0px_0px_#27292b] hover:translate-x-[2px] hover:translate-y-[2px] active:shadow-none active:translate-x-[3px] active:translate-y-[3px] text-sm">
+          <Button className="bg-coral hover:bg-coral/80 text-charcoal font-medium py-1.5 px-3 rounded-md border-2 border-charcoal transition-all duration-200 shadow-[3px_3px_0px_0px_#27292b] hover:shadow-[1px_1px_0px_0px_#27292b] hover:translate-x-[2px] hover:translate-y-[2px] active:shadow-none active:translate-x-[3px] active:translate-y-[3px] text-sm">
             <img src="icons/history.svg" alt="" className="w-6 h-6" />
             <span>History</span>
           </Button>
